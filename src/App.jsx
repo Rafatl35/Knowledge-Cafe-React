920e36e8cfb46c332cfdc18be03166a36021dc54
Rafatl35 <rafatrahmano4@gmail.com>
2024-09-08 22:41:34 +0600

    Display Blog data and hashtags

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <Header></Header>
-      <div className='md:flex md:items-center '>
+      <div className='md:flex'>
         <Blogs></Blogs>
         <Bookmarks></Bookmarks>
       </div>
--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,15 +1,34 @@
 import PropTypes from 'prop-types';
+import './blog.css'
 
-const Blog = ({blog}) => {
-    console.log(blog);
+const Blog = ({ blog }) => {
+    const { Title, Cover, Reading_time, Author_img, Author, Posted_date, Hashtags } = blog;
     return (
-        <div>
-            
+        <div className='mx-6'>
+            <img className='w-full' src={Cover} alt={`Cover picture of the title${Title}`} />
+            <div className='flex justify-between'>
+                <div className='flex items-center'>
+                    <img className='aut-img' src={Author_img} alt="" />
+                    <dir>
+                        <h3 className='text-2xl'>{Author}</h3>
+                        <p>{Posted_date}</p>
+                    </dir>
+                </div>
+                <div>
+                    <span>{Reading_time} min read</span>
+                </div>
+            </div>
+            <h2 className="text-3xl">{Title}</h2>
+            <p>
+                {
+                    Hashtags.map((hash,idx) => <span key={idx}><a href=''>#{hash}</a></span>)
+                }
+            </p>
         </div>
     );
 };
 
-Blog.propTypes ={
+Blog.propTypes = {
     blog: PropTypes.object.isRequired
 }
 export default Blog;
