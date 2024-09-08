7745a89c6fce8b69902fb72a88f2a82716740521
Rafatl35 <rafatrahmano4@gmail.com>
2024-09-08 23:57:41 +0600

    .

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,12 +1,13 @@
 import PropTypes from 'prop-types';
 import './blog.css'
+import { FaRegBookmark } from 'react-icons/fa';
 
 const Blog = ({ blog }) => {
-    const { Title, Cover, Reading_time, Author_img, Author, Posted_date, Hashtags } = blog;
+    const { Title, Cover, Reading_time, Author_img, Author, Posted_date, Hashtags,Description } = blog;
     return (
-        <div className='mx-6'>
-            <img className='w-full' src={Cover} alt={`Cover picture of the title${Title}`} />
-            <div className='flex justify-between'>
+        <div className='mb-20 space-y-3'>
+            <img className='w-full mb-8' src={Cover} alt={`Cover picture of the title${Title}`} />
+            <div className='flex justify-between mb-4'>
                 <div className='flex items-center'>
                     <img className='aut-img' src={Author_img} alt="" />
                     <dir>
@@ -16,9 +17,11 @@ const Blog = ({ blog }) => {
                 </div>
                 <div>
                     <span>{Reading_time} min read</span>
+                    <button className='ml-2'><FaRegBookmark></FaRegBookmark></button>
                 </div>
             </div>
             <h2 className="text-3xl">{Title}</h2>
+            <p>{Description}</p>
             <p>
                 {
                     Hashtags.map((hash,idx) => <span key={idx}><a href=''>#{hash}</a></span>)
